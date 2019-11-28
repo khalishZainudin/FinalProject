5dc022d8b27913c9e3d09680fec64a6c98b0d79b
mnfikry <36684981+mnfikry@users.noreply.github.com>
2019-11-29 07:23:46 +0800

    test

--- app/src/main/java/my/edu/fsktm/um/finalproject/MainActivity.java
+++ app/src/main/java/my/edu/fsktm/um/finalproject/MainActivity.java
@@ -1,17 +1,49 @@
 package my.edu.fsktm.um.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
-    //testing
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     // HELLO WORLD FROM KHALISH
     // HELLO TESTING
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user){
+        if(user==null)
+            mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    }
+                    else{
+                        updateUI(null);
+                    }
+                }
+            });
+    }
 }
---
+++ app/src/main/java/my/edu/fsktm/um/finalproject/TitleForumActivity.java
@@ -0,0 +1,16 @@
+package my.edu.fsktm.um.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class TitleForumActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_forum);
+    }
+}
