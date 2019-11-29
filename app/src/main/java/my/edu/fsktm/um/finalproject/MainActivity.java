014ca02c38d6d39e426692d0601d6182954fec45
khalish <khalish.zainudin@gmail.com>
2019-11-29 09:53:58 +0800

    Commit 3 - khalish - login page - main menu

--- app/src/main/java/my/edu/fsktm/um/finalproject/MainActivity.java
+++ app/src/main/java/my/edu/fsktm/um/finalproject/MainActivity.java
@@ -2,15 +2,36 @@ package my.edu.fsktm.um.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import my.edu.fsktm.um.finalproject.LoginPage.EmailPassActivity;
 
 public class MainActivity extends AppCompatActivity {
-    //testing
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout mylayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mylayout = (RelativeLayout) findViewById(R.id.my_bg_layout);
+        animationDrawable = (AnimationDrawable) mylayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
 
     // HELLO WORLD FROM KHALISH
+
+    public void goToLoginPage(View v) {
+        Intent intent = new Intent(this, EmailPassActivity.class);
+        startActivity(intent);
+    }
 }
